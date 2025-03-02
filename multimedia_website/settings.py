95a8043156bb79f3baca8e98ff44f5a09c3b89cc
Horlarmmy <aladetoheeb5@gmail.com>
2025-03-02 22:32:19 +0100

    Styled the page and new update

--- multimedia_website/settings.py
+++ multimedia_website/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1494sd*db7b^!+$xlb-smn^@fvy8rud@w@jf4unq!%*oq_b31u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = 'True'
 
 ALLOWED_HOSTS = ['*']
 
